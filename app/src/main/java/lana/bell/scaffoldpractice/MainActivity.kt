0dc09cbea1b4b8dda88c06a87132a4ea30d9bc8c
alanahbell10 <abell22@students.towson.edu>
2022-11-06 10:14:02 -0500

    initiliazed

---
+++ app/src/main/java/lana/bell/scaffoldpractice/MainActivity.kt
@@ -0,0 +1,43 @@
+package lana.bell.scaffoldpractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import lana.bell.scaffoldpractice.ui.theme.ScaffoldPracticeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScaffoldPracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ScaffoldPracticeTheme {
+        Greeting("Android")
+    }
+}
